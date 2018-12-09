a4bd7865193739e8df369d830ef932f6d41bd432
xujin <Software_King@qq.com>
2018-12-09 21:19:34 +0800

    Add data

--- sc-gateway-server/src/main/java/cn/springcloud/book/gateway/route/DynamicRouteServiceImplByNacos.java
+++ sc-gateway-server/src/main/java/cn/springcloud/book/gateway/route/DynamicRouteServiceImplByNacos.java
@@ -2,7 +2,6 @@ package cn.springcloud.book.gateway.route;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.nacos.api.NacosFactory;
-import com.alibaba.nacos.api.PropertyKeyConst;
 import com.alibaba.nacos.api.config.ConfigService;
 import com.alibaba.nacos.api.config.listener.Listener;
 import com.alibaba.nacos.api.exception.NacosException;
@@ -10,7 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.gateway.route.RouteDefinition;
 import org.springframework.stereotype.Component;
 
-import java.util.Properties;
 import java.util.concurrent.Executor;
 
 @Component
@@ -20,15 +18,18 @@ public class DynamicRouteServiceImplByNacos {
     private DynamicRouteServiceImpl dynamicRouteService;
 
     public DynamicRouteServiceImplByNacos() {
-        dynamicRouteBylistenerNacos("sc-gateway","xujin_test");
+
+        dynamicRouteByNacosListener("sc-gateway","xujin_test");
     }
-    public void dynamicRouteBylistenerNacos (String dataId, String group){
+
+    /**
+     * 监听Nacos Server下发的动态路由配置
+     * @param dataId
+     * @param group
+     */
+    public void dynamicRouteByNacosListener (String dataId, String group){
         try {
-            Properties properties = new Properties();
-            //properties.put("nacos.server-addr", "");
-            properties.put(PropertyKeyConst.SERVER_ADDR, "127.0.0.1:8848");
-            ConfigService configService=NacosFactory.createConfigService(properties);
-            //ConfigService configService=NacosFactory.createConfigService("127.0.0.1:8848");
+            ConfigService configService=NacosFactory.createConfigService("127.0.0.1:8848");
             String content = configService.getConfig(dataId, group, 5000);
             System.out.println(content);
             configService.addListener(dataId, group, new Listener()  {
@@ -43,7 +44,7 @@ public class DynamicRouteServiceImplByNacos {
                 }
             });
         } catch (NacosException e) {
-            e.printStackTrace();
+            //todo 提醒:异常自行处理此处省略
         }
     }
 
